f30425d798a6df9919be16b5095efc94e83f626b
Alexander Sage <alexander.sage@uniqfeed.com>
2019-04-18 14:57:31 +0200

    initial commit

---
+++ LED_Tester/src/main.cpp
@@ -0,0 +1,48 @@
+#include <Arduino.h>
+#include <PololuLedStrip.h>
+
+// Create an ledStrip object and specify the pin it will use.
+PololuLedStrip<12> ledStrip;
+
+// Create a buffer for holding the colors (3 bytes per color).
+#define LED_COUNT 60*5
+rgb_color colors[LED_COUNT];
+uint16_t count;
+
+// Converts a color from HSV to RGB.
+// h is hue, as a number between 0 and 360.
+// s is the saturation, as a number between 0 and 255.
+// v is the value, as a number between 0 and 255.
+rgb_color hsvToRgb(uint16_t h, uint8_t s, uint8_t v)
+{
+    uint8_t f = (h % 60) * 255 / 60;
+    uint8_t p = (255 - s) * (uint16_t)v / 255;
+    uint8_t q = (255 - f * (uint16_t)s / 255) * (uint16_t)v / 255;
+    uint8_t t = (255 - (255 - f) * (uint16_t)s / 255) * (uint16_t)v / 255;
+    uint8_t r = 0, g = 0, b = 0;
+    switch((h / 60) % 6){
+        case 0: r = v; g = t; b = p; break;
+        case 1: r = q; g = v; b = p; break;
+        case 2: r = p; g = v; b = t; break;
+        case 3: r = p; g = q; b = v; break;
+        case 4: r = t; g = p; b = v; break;
+        case 5: r = v; g = p; b = q; break;
+    }
+    return rgb_color(r, g, b);
+}
+
+void setup() {
+  // put your setup code here, to run once:
+  count = 0;
+}
+
+void loop() {
+  for(uint16_t i = 0; i < LED_COUNT; i++)
+  {
+    colors[i] = rgb_color(255, 255, 255);
+  }
+  
+  ledStrip.write(colors, LED_COUNT);
+
+  delay(100);
+}
